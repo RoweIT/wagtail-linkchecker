c6fea23ce119620cc9834dcb5774e1b92aed4f51
Adam Nicholas <adam.nicholas@roweit.co.uk>
2020-02-26 14:59:35 +0000

    Corrected superuser check

--- wagtaillinkchecker/wagtail_hooks.py
+++ wagtaillinkchecker/wagtail_hooks.py
@@ -29,7 +29,9 @@ def register_admin_urls():
 
 @hooks.register('register_settings_menu_item')
 def register_menu_settings(user):
-    if not user.is_superuser:
+    if user.is_superuser:
+        shown=True
+    else:
         shown=False
 
     return MenuItem(
